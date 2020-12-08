95881029400a094c8160ac2f0a95245707eb2d9b
JurijOrlow <32055243+JurijOrlow@users.noreply.github.com>
2020-12-08 11:18:37 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script for configuration of Manjaro Linux Gnome edition with I3 Window Manager
+# Notice - you can't run this script as superuser (sudo)
+# But several times you will be prompted about sudo password
+
+user="orlow"
+
+echo "Creating configuration folder in $user home directory"
+
+cd /home/$user
+mkdir ConfigFiles
+cd ./ConfigFiles
+
+echo "Downloading git and base-devel"
+
+sudo pacman -S base-devel git
+
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $user:$user ./yay-git
+
+cd yay-git ; makepkg -si
+
+echo "Updating your system and packages"
+
+yay
+
+echo "Installing I3 and necessary applications"
+
+yay -S i3 rofi vim dmenu arandr feh xbacklight
+
+echo "Done, reboot now, log into I3 window manager and run i3config.sh"
