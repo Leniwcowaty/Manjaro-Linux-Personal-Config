a7549425c72c1ad76f89e569d7edb796e9af0297
JurijOrlow <32055243+JurijOrlow@users.noreply.github.com>
2020-12-08 13:39:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ yay
 
 echo "Installing I3 and necessary applications"
 
-yay -S i3 rofi vim dmenu arandr feh xorg-xbacklight google-chrome vlc spotify mc tree lxappearance
+yay -S i3 rofi vim dmenu arandr feh xorg-xbacklight google-chrome vlc spotify mc tree lxappearance htop neofetch
 
 echo "Done, reboot now, log into I3 window manager and run i3config.sh"
 echo "After logging into I3 DO NOT autocreate config file (press Esc on pop-up window)"
