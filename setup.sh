8b696179d74f25384a5c1d78962efc494946d429
JurijOrlow <32055243+JurijOrlow@users.noreply.github.com>
2020-12-12 12:01:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@
 echo "Input your user name"
 read user
 
+echo "Input your \"Pictures\" directory name"
+read pictures
+
 echo "Creating configuration folder in $user home directory"
 
 cd /home/$user
@@ -27,7 +30,19 @@ yay
 
 echo "Installing I3 and necessary applications"
 
-yay -S i3 rofi vim dmenu arandr feh xorg-xbacklight google-chrome vlc spotify mc tree lxappearance htop neofetch tmux flameshot xorg-xprop thunderbird
+yay -S vim google-chrome vlc spotify mc tree htop neofetch tmux thunderbird
+
+echo "Input your \"Pictures\" directory name"
+read pictures
+
+echo "Copying configuration files and wallpapers"
+
+cp ./i3config/lock_screen.png /home/$user/$pictures/lock_screen.png
+
+unzip themes.zip
+sudo cp -r ./themes/FossaCursors /usr/share/icons
+sudo cp -r ./themes/Simply-White-Circles /usr/share/icons
+sudo cp -r ./themes/Simply_Circles_Dark_Envolved /usr/share/themes
 
 echo "Done, reboot now, log into I3 window manager and run i3config.sh"
 echo "After logging into I3 DO NOT autocreate config file (press Esc on pop-up window)"
